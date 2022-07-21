cd696dc26fb9679dda1e548a6b035b0465b9af14
Christian Schwanse <Christianschwanse@gmx.de>
2022-07-21 15:39:39 +0200

    Build: Dao-queries to manipulate diet entries in database

--- app/src/main/java/com/example/fastingtracker/database/progress/DietDao.kt
+++ app/src/main/java/com/example/fastingtracker/database/progress/DietDao.kt
@@ -1,11 +1,28 @@
 package com.example.fastingtracker.database.progress
 
-import androidx.room.Dao
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Contains SQL commands/queries that belong to the "progress" section of the app.
  */
 @Dao
 interface DietDao {
-    // TODO: Implement Queries in DietDao
+    // ------------------------ SQL-commands to modify an entry ------------------------
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(entry: Diet)
+
+    @Update
+    suspend fun update(entry: Diet)
+
+    @Delete
+    suspend fun delete(entry: Diet)
+
+    // ------------------------ Get entries ------------------------
+    @Query("SELECT * FROM diets WHERE id = :id")
+    fun getEntry(id: Int): Flow<Diet>
+
+    // Latest entry should be displayed first
+    @Query("SELECT * FROM diets ORDER BY time_of_measurement DESC")
+    fun getAllEntries(): Flow<List<Diet>>
 }
